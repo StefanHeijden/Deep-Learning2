988f27e77023a151437ec134a9adad89dfea49be
stefan <stefanheijden1@gmail.com>
2019-03-27 11:01:52 +0100

    opdracht 3

--- src/main/java/experiments/FunctionExperiment.java
+++ src/main/java/experiments/FunctionExperiment.java
@@ -7,24 +7,38 @@ import nl.tue.s2id90.dl.NN.Model;
 import nl.tue.s2id90.dl.NN.layer.InputLayer;
 import nl.tue.s2id90.dl.NN.tensor.TensorShape;
 import nl.tue.s2id90.dl.NN.layer.SimpleOutput;
+import nl.tue.s2id90.dl.NN.layer.FullyConnected;
 import nl.tue.s2id90.dl.NN.loss.MSE;
+import nl.tue.s2id90.dl.NN.activation.RELU;
 import nl.tue.s2id90.dl.NN.initializer.Gaussian;
 import nl.tue.s2id90.dl.NN.optimizer.Optimizer;
 import nl.tue.s2id90.dl.NN.optimizer.SGD;
 import nl.tue.s2id90.dl.NN.validate.Regression;
+import nl.tue.s2id90.dl.experiment.GUIExperiment;
 
-public class FunctionExperiment extends Experiment {
+public class FunctionExperiment extends GUIExperiment {
     // ( hyper ) parameters
-    int epochs=10; 
+    int batchSize = 32;
+    // The parameter epochs is the number of epochs that a
+    // training takes. In an epoch all the training samples are presented
+    // once to the neural network.
+    int epochs = 1; 
+    // Parameter for the gradient descent optimization method.
     double learningRate = 0.01;
     
     // normal parameters
-    int batchSize = 32;
-    InputReader reader = GenerateFunctionData .THREE_VALUED_FUNCTION( batchSize ) ;
+    // the number of incoming connections
+    int m = 1;
+    // the number of neurons of the new layer
+    int n = 10;
+    InputReader reader = GenerateFunctionData .THREE_VALUED_FUNCTION( batchSize );
     int inputs = reader.getInputShape().getNeuronCount();
     int outputs = reader.getOutputShape().getNeuronCount();
         
     public void go( ) throws IOException {
+        System.out.println ("Inputs: " + inputs ) ;
+         System.out.println ("Outputs: " + outputs ) ;
+         
         Model model = createModel ( inputs , outputs ) ;
         model. initialize (new Gaussian());
         // Training : c r e a t e and conf igure SGD && t r a i n model
@@ -38,7 +52,7 @@ public class FunctionExperiment extends Experiment {
        
         // read input and pr int some informat ion on the data
         System.out.println ("Reader info:\n" + reader.toString( ) ) ;
-        reader.getValidationData(10).forEach(System.out::println);
+        reader.getValidationData(20).forEach(System.out::println);
     }
     
     public static void main ( String[ ] args ) throws IOException {
@@ -47,7 +61,8 @@ public class FunctionExperiment extends Experiment {
     
     Model createModel(int inputs , int outputs ) {
         Model model = new Model(new InputLayer("In", new TensorShape(inputs ) , true ) ) ;
-        model.addLayer(new SimpleOutput("Out ", new TensorShape(inputs ) , outputs , new MSE() , true ) ) ;
+        model.addLayer(new FullyConnected ( "fc1" , new TensorShape(inputs) , n , new RELU( )) ) ;
+        model.addLayer(new SimpleOutput("Out ", new TensorShape(n) , outputs , new MSE() , true ) ) ;
         return model;
     }
 }
